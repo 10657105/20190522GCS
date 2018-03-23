34abf64fcb77331314d8eca0acd89020e36df72e
ShengYu <10657105@example.com>
2018-03-23 20:56:38 +0800

    battery_voltage

--- GCSViews/ConnectionData.Designer.cs
+++ GCSViews/ConnectionData.Designer.cs
@@ -67,7 +67,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(141, 67);
             this.label1.TabIndex = 0;
-            //this.label1.Text = hud1.batterylevel.ToString();
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
--- GCSViews/ConnectionData.cs
+++ GCSViews/ConnectionData.cs
@@ -8,23 +8,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MissionPlanner.Controls;
+using System.Threading;
 
 namespace MissionPlanner.GCSViews
 {
     public partial class ConnectionData : MyUserControl
     {
-        public static HUD hud;
+        public static CurrentState curs;
+        //public static HUD ;
         public ConnectionData()
         {
             InitializeComponent();
-            hud = new HUD();
             /*try
             {
                 label1.Text = hud.batterylevel.ToString();
             }
             catch { }*/
+            
+        }
+        private void PrintBat()
+        {
+            //curs = new CurrentState();
+            while (true)
+            {
+                this.Invoke(new Action(delegate ()
+                {
+                    label1.Text = MainV2.comPort.MAV.cs.battery_voltage.ToString();
+                }));
+            }
         }
 
-
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Thread ThreadA = new Thread(new ThreadStart(PrintBat));
+            ThreadA.Name = "AThread";
+            ThreadA.IsBackground = true;
+            ThreadA.Start();
+        }
     }
 }
