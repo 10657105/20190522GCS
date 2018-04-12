df79d0553875b2389768a2352886a7e128a354f9
ShengYu <ShengYu@DESKTOP-ITIL5CD>
2018-04-12 20:28:35 +0800

    新增data資料

--- GCSViews/ConnectionData.Designer.cs
+++ GCSViews/ConnectionData.Designer.cs
@@ -29,12 +29,25 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.connectionStatus1 = new MissionPlanner.GCSViews.ConnectionStatus();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -42,11 +55,11 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.connectionStatus1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -55,26 +68,17 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(193, 337);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(220, 337);
             this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 67);
-            this.label1.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Location = new System.Drawing.Point(3, 67);
+            this.label2.Location = new System.Drawing.Point(3, 79);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(187, 67);
+            this.label2.Size = new System.Drawing.Size(214, 79);
             this.label2.TabIndex = 0;
             this.label2.Text = "label2";
             // 
@@ -82,9 +86,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(3, 134);
+            this.label3.Location = new System.Drawing.Point(3, 158);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(187, 67);
+            this.label3.Size = new System.Drawing.Size(214, 79);
             this.label3.TabIndex = 0;
             this.label3.Text = "label3";
             // 
@@ -92,15 +96,138 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label5.Location = new System.Drawing.Point(3, 268);
+            this.label5.Location = new System.Drawing.Point(3, 316);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(187, 69);
+            this.label5.Size = new System.Drawing.Size(214, 21);
             this.label5.TabIndex = 0;
             this.label5.Text = "label5";
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(214, 73);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48F));
+            this.tableLayoutPanel3.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label6, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label8, 2, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 27);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(208, 18);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "label4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label6.Location = new System.Drawing.Point(69, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 18);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "label6";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = global::MissionPlanner.Properties.Resources.satellite;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(12, 13);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label8.Location = new System.Drawing.Point(110, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 18);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "label8";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 3;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel4.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label7, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label9, 2, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 51);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(208, 19);
+            this.tableLayoutPanel4.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label7.Location = new System.Drawing.Point(3, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 19);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "V A";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label9.Location = new System.Drawing.Point(127, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 19);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Sats";
+            // 
             // connectionStatus1
             // 
-            this.connectionStatus1.Location = new System.Drawing.Point(3, 204);
+            this.connectionStatus1.Location = new System.Drawing.Point(3, 240);
             this.connectionStatus1.Name = "connectionStatus1";
             this.connectionStatus1.Size = new System.Drawing.Size(141, 61);
             this.connectionStatus1.TabIndex = 1;
@@ -111,10 +238,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ConnectionData";
-            this.Size = new System.Drawing.Size(193, 337);
+            this.Size = new System.Drawing.Size(220, 337);
             this.Load += new System.EventHandler(this.ConnectionData_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -122,10 +256,19 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private ConnectionStatus connectionStatus1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
     }
 }
--- GCSViews/ConnectionData.cs
+++ GCSViews/ConnectionData.cs
@@ -9,13 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MissionPlanner.Controls;
 using System.Threading;
+using System.Drawing;
 
 namespace MissionPlanner.GCSViews
 {
     public partial class ConnectionData : MyUserControl
     {
         public static CurrentState curs;
-        //public static HUD ;
+        
         public ConnectionData()
         {
             InitializeComponent();
@@ -30,10 +31,57 @@ namespace MissionPlanner.GCSViews
                 if (MainV2.comPort.BaseStream.IsOpen)
                 {
                     this.Invoke(new Action(delegate ()
-                {
-                    label1.Text = MainV2.comPort.MAV.cs.battery_voltage.ToString();
-                }));
+                    {
+                        if (MainV2.Comports[0] != null)
+                        {
+                            label7.Text = MainV2.Comports[0].MAV.cs.battery_voltage.ToString("f1") + "V   " + MainV2.Comports[0].MAV.cs.current.ToString("f1") + "A ";                                       
+                            label1.Text = MainV2.Comports[0].MAV.cs.alt.ToString("f1") + "m   " + MainV2.Comports[0].MAV.cs.yaw.ToString("f1") + "deg   " 
+                                        + MainV2.Comports[0].MAV.cs.groundspeed.ToString("f1") + "m/s   " + MainV2.Comports[0].MAV.cs.mode.ToString() + " ";
+                            label9.Text = MainV2.Comports[0].MAV.cs.satcount.ToString() + "   (" + MainV2.Comports[0].MAV.cs.gpshdop.ToString() + "m)  ";
+
+                            if (MainV2.Comports[0].MAV.cs.armed)
+                            {//arm
+                                label4.Text = "Armed";
+                                label4.ForeColor = Color.Red;
+                            }
+                            else
+                            {
+                                label4.Text = "Disarmed";
+                                label4.ForeColor = Color.Lime;
+                            }
+
+                             if (MainV2.Comports[0].MAV.cs.ekfstatus > 0.5)
+                             {//EKF
+                                 if (MainV2.Comports[0].MAV.cs.ekfstatus > 0.8)
+                                 {
+                                   label6.Text = "EKF";
+                                   label6.ForeColor = Color.Red;
+                                 }
+                                 else
+                                 {
+                                    label6.Text = "EKF";
+                                    label6.ForeColor = Color.Orange;
+                                 }
+                             }
+                             else
+                             {
+                                label6.Text = "EKF";
+                                label6.ForeColor = Color.Lime;
+                            }
+                            label8.Text = MainV2.Comports[0].MAV.cs.linkqualitygcs.ToString() + "% " + MainV2.Comports[0].ToString().Remove(0,9);
+                        }
+                        if (MainV2.Comports.Count > 1 && MainV2.Comports[1] != null)
+                        {
+                          label2.Text = MainV2.Comports[1].MAV.cs.battery_voltage.ToString("f1") + "V  " + MainV2.Comports[1].MAV.cs.current.ToString("f1") + "A  \n"
+                                      + MainV2.Comports[1].MAV.cs.alt.ToString("f1") + "m  " + MainV2.Comports[1].MAV.cs.yaw.ToString("f1") + "deg  " + MainV2.Comports[1].MAV.cs.mode.ToString() + " \n"
+                                      + "Ssta:" + MainV2.Comports[1].MAV.cs.satcount.ToString() + "  " + "hdop:" + MainV2.Comports[1].MAV.cs.gpshdop.ToString(); ;
+                        }
+                       
+
+                    }));
+
                 }
+
             }
         }
 
@@ -49,6 +97,11 @@ namespace MissionPlanner.GCSViews
             Thread ThreadA = new Thread(new ThreadStart(PrintBat));
             ThreadA.IsBackground = true;
             ThreadA.Start();
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
